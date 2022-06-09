75a5f85c98ab0db8db3fc1098ed00d396f95c4fd
Nikesh Mistri <nikeshmistri@hotmail.co.uk>
2022-06-09 16:23:35 +0100

    setup

---
+++ script.js
@@ -0,0 +1,8 @@
+
+async function  getCountryByName(countryName) {
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then(response =>{ return response.json()})
+    .then(data => {console.log(data)});
+}
+
+getCountryByName("Peru");
